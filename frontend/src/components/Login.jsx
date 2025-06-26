e4f13a1054f5a7cd583e3f2db3ce80586ad0ba5d
Abhi-A007 <abhirupray.somu@gmail.com>
2025-06-26 12:25:16 +0530

    login func

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { login } from "../supabase";
 
 function Login() {
+  const [credential, setCredential] = useState({ email: "", password: "" });
+
+  function handleChange(event) {
+    const { type, value } = event.target;
+    setCredential((prevData) => ({
+      ...prevData,
+      [type]: value,
+    }));
+  }
+
+  async function handleSubmit(event) {
+      event.preventDefault(); 
+      const { data, error } = await login(credential.email, credential.password);
+      console.log("Login Data:", data);
+      console.log("Login Error:", error);
+    }
+
   return (
     <div id="form">
-      <form className="signup-form">
+      <form className="signup-form" onSubmit={handleSubmit}>
         <h2>Log In</h2>
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
+        <input
+          onChange={handleChange}
+          type="email"
+          placeholder="Email"
+          value={credential.email}
+          required
+        />
+        <input
+          onChange={handleChange}
+          type="password"
+          placeholder="Password"
+          value={credential.password}
+          required
+        />
         <button type="submit">Login</button>
       </form>
     </div>
