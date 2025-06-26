e8ccaacc32aa48fbf8fdf572e6764d4fe2c24823
Soudeepan <soudeepanbiswas@gmail.com>
2025-06-26 16:18:34 +0530

    hell

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {logOut} from "../supabase"
+import {logOut, getAllItems, getAllMyItems, getAllMyOrders} from "../supabase"
 
 function Dashboard() {
     const navigate = useNavigate();
