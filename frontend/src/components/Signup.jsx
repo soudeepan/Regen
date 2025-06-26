40ab1324068ade46f2363277433bdb8f40f4816e
Abhi-A007 <abhirupray.somu@gmail.com>
2025-06-26 11:58:52 +0530

    SignUp func

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import signUp from "../supabase";
 
 function Signup() {
+  const [credential, setCredential] = useState({ email: "", password: "" });
+
+  function handleChange(event) {
+    const { type, value } = event.target;
+    setCredential((prevData) => {
+      return {
+        ...prevData,
+        [type]: value,
+      };
+    });
+  }
+
   return (
     <div id="form">
       <form className="signup-form">
         <h2>Sign Up</h2>
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
-        <button type="submit">Create Account</button>
+        <input
+          onChange={handleChange}
+          type="email"
+          placeholder="Email"
+          value={credential.email}
+          required
+        />
+        <input
+          onChange={handleChange}
+          type="password"
+          placeholder="Password"
+          value={credential.password}
+          required
+        />
+        <button
+          onClick={() => {
+            signUp(credential.email, credential.password);
+          }}
+          type="submit"
+        >
+          Create Account
+        </button>
       </form>
     </div>
   );
